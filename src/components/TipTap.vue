8691f52c08e2cbe71b259e8a742426887c6f9360
Nur Muhammad <blog.nurmuhammad@gmail.com>
2022-12-05 13:44:00 +0800

    use sticky for toolbar

--- src/components/TipTap.vue
+++ src/components/TipTap.vue
@@ -118,106 +118,108 @@ const can = (action) => {
       <material-icon name="format_underline" size="20" />
     </button>
   </bubble-menu>
-  <div v-if="editor" class="toolbar">
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ 
-          active: editor.getAttributes('textClass')?.class?.includes('text-arabic')
-        }"
-        @click="format('textClass', 'text-arabic')">
-        <img src="../assets/icons/abjad_arabic_icon.svg" height="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ active: isActive('bold') }"
-        @click="format('bold')">
-        <material-icon name="format_bold" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive('italic') }"
-        @click="format('italic')">
-        <material-icon name="format_italic" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive('underline') }"
-        @click="format('underline')">
-        <material-icon name="format_underline" size="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ active: isActive({ textAlign: 'left' }) }"
-        @click="format('set-text-align', 'left')">
-        <material-icon name="format_align_left" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive({ textAlign: 'center' }) }"
-        @click="format('set-text-align', 'center')">
-        <material-icon name="format_align_center" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive({ textAlign: 'right' }) }"
-        @click="format('set-text-align', 'right')">
-        <material-icon name="format_align_right" size="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ active: isActive('superscript') }"
-        @click="format('superscript')">
-        <material-icon name="superscript" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive('subscript') }"
-        @click="format('subscript')">
-        <material-icon name="subscript" size="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ active: isActive('bullet-list') }"
-        @click="format('bullet-list')">
-        <material-icon name="format_list_bulleted" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        :class="{ active: isActive('ordered-list') }"
-        @click="format('ordered-list')">
-        <material-icon name="format_list_numbered" size="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        :class="{ active: isActive('blockquote') }"
-        @click="format('blockquote')">
-        <material-icon name="format_quote" size="20" />
-      </button>
-    </div>
-    <div class="menu-group">
-      <button
-        class="menu-item"
-        @click="format('undo')"
-        :disabled="!can('undo')">
-        <material-icon name="undo" size="20" />
-      </button>
-      <button
-        class="menu-item"
-        @click="format('redo')"
-        :disabled="!can('redo')">
-        <material-icon name="redo" size="20" />
-      </button>
+  <div v-if="editor" class="toolbar-container">
+    <div class="toolbar">
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ 
+            active: editor.getAttributes('textClass')?.class?.includes('text-arabic')
+          }"
+          @click="format('textClass', 'text-arabic')">
+          <img src="../assets/icons/abjad_arabic_icon.svg" height="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ active: isActive('bold') }"
+          @click="format('bold')">
+          <material-icon name="format_bold" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive('italic') }"
+          @click="format('italic')">
+          <material-icon name="format_italic" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive('underline') }"
+          @click="format('underline')">
+          <material-icon name="format_underline" size="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ active: isActive({ textAlign: 'left' }) }"
+          @click="format('set-text-align', 'left')">
+          <material-icon name="format_align_left" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive({ textAlign: 'center' }) }"
+          @click="format('set-text-align', 'center')">
+          <material-icon name="format_align_center" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive({ textAlign: 'right' }) }"
+          @click="format('set-text-align', 'right')">
+          <material-icon name="format_align_right" size="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ active: isActive('superscript') }"
+          @click="format('superscript')">
+          <material-icon name="superscript" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive('subscript') }"
+          @click="format('subscript')">
+          <material-icon name="subscript" size="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ active: isActive('bullet-list') }"
+          @click="format('bullet-list')">
+          <material-icon name="format_list_bulleted" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          :class="{ active: isActive('ordered-list') }"
+          @click="format('ordered-list')">
+          <material-icon name="format_list_numbered" size="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          :class="{ active: isActive('blockquote') }"
+          @click="format('blockquote')">
+          <material-icon name="format_quote" size="20" />
+        </button>
+      </div>
+      <div class="menu-group">
+        <button
+          class="menu-item"
+          @click="format('undo')"
+          :disabled="!can('undo')">
+          <material-icon name="undo" size="20" />
+        </button>
+        <button
+          class="menu-item"
+          @click="format('redo')"
+          :disabled="!can('redo')">
+          <material-icon name="redo" size="20" />
+        </button>
+      </div>
     </div>
   </div>
   <div class="editor-container">
