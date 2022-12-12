5c820aea4c462d5d2b04a6c2710636730bb97ccb
Nur Muhammad <blog.nurmuhammad@gmail.com>
2022-12-12 10:33:11 +0800

    add share feature

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted, nextTick } from 'vue'
 import { toBlob } from 'html-to-image'
 import { saveAs } from 'file-saver'
-import { useStorage } from '@vueuse/core'
+import { useStorage, useShare } from '@vueuse/core'
 import TipTap from './components/TipTap.vue'
 import InstallPrompt from './components/InstallPrompt.vue'
+import PreviewLoader from 'vue-spinner/src/BeatLoader.vue'
 import UAParser from 'ua-parser-js'
 
 const content = useStorage('berbagi-catatan-content', '')
 
 const previews = ref([])
+const blobPreviews = ref([])
 const filename = ref('berbagi-catatan')
-const previewsSaving = ref([])
 
 const frameCornerContainer = ref(null)
+
+const sortedBlobPreviews = computed(() => {
+  return [...blobPreviews.value].sort((a, b) => a.index - b.index)
+})
 
 onMounted(() => {
   frameCornerContainer.value = document.querySelector('.frame-corner-container').outerHTML
 })
 
-const buildPreviews = () => {
+const buildBlob = async (el) => {
+  return await toBlob(el, {
+    pixelRatio: 2.5,
+    style: {
+      margin: 0,
+    }
+  })
+}
+
+const buildPreviews = async () => {
   const el = document.querySelector('.editor-container .ProseMirror')
 
   const str = el.innerHTML
   const elements = str.split(/<hr\s+[^>]*>/g)
 
-  previewsSaving.value = Array(elements.length).fill(false)
   previews.value = elements
 
   // Go to top
@@ -33,58 +46,100 @@ const buildPreviews = () => {
     top: 0,
     behavior: 'smooth'
   })
+
+  await nextTick()
+  
+  /**
+   * For easy download and share
+   * We directly generate and store the blob file
+   */
+  blobPreviews.value = [] // Reset
+  for (let index = 0; index < previews.value.length; index++) {
+    const previewSelector = `.preview-result-item-${index}`
+    const attribution = document.querySelector(`${previewSelector} footer`)
+
+    // Show attribution
+    attribution.style.display = 'block'
+
+    const el = document.querySelector(previewSelector)
+
+    /**
+     * We need to run it twice to get arabic font
+     * Tested on iOS (Chrome & Safari) - WebKit engine
+     */
+    const ua = UAParser()
+
+    if (ua.engine.name == 'WebKit') {
+      buildBlob(el).then(() => {
+        buildBlob(el).then(blob => {
+          blobPreviews.value.push({
+            index,
+            blob
+          })
+
+          // Hide attribution
+          attribution.style.display = 'none'
+        })
+      })
+    } else {
+      buildBlob(el).then(blob => {
+        blobPreviews.value.push({
+          index,
+          blob
+        })
+
+        // Hide attribution
+        attribution.style.display = 'none'
+      })
+    }
+  }
 }
 
 const resetPreviews = () => {
   previews.value = []
 }
 
-const save = async (selector, index) => {
-  previewsSaving.value[index] = true
+const getFilename = (index) => {
+  return (filename.value.length ? filename.value : 'berbagi-catatan') +
+          (previews.value.length > 1 ? '-' + (index + 1) : '') +
+          '.png'
+}
 
-  const previewSelector = `${selector}-${index}`
-  const attribution = document.querySelector(`${previewSelector} footer`)
-
-  // Show attribution
-  attribution.style.display = 'block'
-
-  const name = (filename.value.length ? filename.value : 'berbagi-catatan') +
-                (previews.value.length > 1 ? '-' + (index + 1) : '')
-
-  const SCALE = 2.5
-  const el = document.querySelector(previewSelector)
-
-  /**
-   * We need to run it twice to get arabic font
-   * Tested on iOS (Chrome & Safari) - WebKit engine
-   */
-  const ua = UAParser()
-
-  if (ua.engine.name == 'WebKit') {
-    await toBlob(el, {
-      pixelRatio: SCALE,
-      style: {
-        margin: 0,
-      }
-    })
-  }
-  
-  toBlob(el, {
-    pixelRatio: SCALE,
-    style: {
-      margin: 0,
-    }
-  }).then((blob) => {
-    saveAs(blob, name + '.png')
-    previewsSaving.value[index] = false
-    
-    // Hide attribution
-    attribution.style.display = 'none'
-  })
+const save = (index) => {
+  const name = getFilename(index)
+  const preview = blobPreviews.value.find(preview => preview.index == index)
+  saveAs(preview.blob, name)
 }
 
 const reset = () => {
   content.value = ''
+}
+
+const { share, isSupported: isShareSupported } = useShare()
+
+const createFile = (blob, name) => {
+  return new File([blob], name, {
+    type: blob.type,
+    lastModified: new Date().getTime()
+  })
+}
+
+// Share all when the index is undefined
+const doShare = (index = undefined) => {
+  const files = []
+
+  if (index !== undefined) {
+    const preview = blobPreviews.value.find(preview => preview.index == index)
+    const file = createFile(preview.blob, getFilename(index))
+    files.push(file)
+  } else {
+    sortedBlobPreviews.value.forEach(preview => {
+      const file = createFile(preview.blob, getFilename(preview.index))
+      files.push(file)
+    })
+  }
+
+  share({ files })
 }
 </script>
 
@@ -109,7 +164,12 @@ const reset = () => {
     </footer>
   </template>
   <!-- Result preview -->
-  <div v-else class="preview">
+  <div v-else class="preview" :class="{ 'with-share-all': previews.length > 1 }">
+    <!-- Overlay loading -->
+    <div v-if="previews.length != blobPreviews.length" class="preview-loading">
+      <preview-loader color="#7D7268" />
+      <p class="message">Menyiapkan pratinjau...</p>
+    </div>
     <header class="preview-header">
       <div class="preview-header--title">
         <button class="btn-back" @click="resetPreviews()">
@@ -140,14 +200,26 @@ const reset = () => {
           </footer>
         </div>
         <div class="download-container">
-          <button 
-            class="btn-download"
-            :class="{ loading: previewsSaving[index] }"
-            @click="save('.preview-result-item', index)">
-            {{ previewsSaving[index] ? 'Loading...' : 'Unduh Gambar' }}
-          </button>
+          <div :class="{ 'btn-group': isShareSupported }">
+            <button class="btn btn-sm" @click="save(index)">
+              <material-icon name="download" size="16" />
+              <span>Unduh</span>
+            </button>
+            <button v-if="isShareSupported" class="btn-secondary btn-sm" @click="doShare(index)">
+              <material-icon name="share" size="16" />
+              <span>Bagikan</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
+    <!-- Share all -->
+    <button 
+      v-if="isShareSupported && previews.length > 1" 
+      class="btn-share-all"
+      @click="doShare()">
+      <material-icon name="share" size="20" />
+      <span>Bagikan Semua</span>
+    </button>
   </div>
 </template>
