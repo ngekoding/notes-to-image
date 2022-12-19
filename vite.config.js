88f8234d7805925e13d86aa00b97d9806dc479ab
Nur Muhammad <blog.nurmuhammad@gmail.com>
2022-12-20 05:43:44 +0800

    add path alias: @ as /src

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -64,5 +65,10 @@ export default defineConfig(({ mode }) => ({
         ]
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 }))
